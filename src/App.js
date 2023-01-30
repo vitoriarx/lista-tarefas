b14716efa0749267e0bf1f68cc348f686112a2ac
Vitória Melo <90222577+vitoriarx@users.noreply.github.com>
2023-01-30 15:48:46 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React, {useState, useRef} from "react";
 import Icone from "./imagem/icone.png";
+import Lixeira from "./imagem/trash.svg"
 
-import {Container, H1, Label, Input, Button, Image} from './styles';
+import {Container, H1, Input, Button, Image, ContainerItens, Tarefa } from './styles';
 
-const tarefas = [
+function App () {
+  const [tarefas, setTarefas] = useState([]);
+  const inputTitulo = useRef()
+  const inputSobre = useRef()
 
-];
-
-function App() {
-  
-  const [tarefa, setUsers] = useState([]);
-  const titulo = useRef()
-  const sobre = useRef()
-
-  function addNovaTarefa(){
-    setUsers([... tarefas, {id: Math.random(), name:titulo.current.value, age:sobre.current.value}])
-  }
-
- 
-  return (
-    <Container>
-      <Image src={Icone}/>
-
-      <H1>Lista de Tarefas</H1>
-
-      <Label></Label>
-      <Input ref={titulo} type='text' placeholder="Título"></Input>
-
-      <Label> </Label>
-      <Input ref={sobre} type='text' placeholder="Sobre"></Input>
-
-      <Button>Cadastrar Tarefa</Button>
-
-      
-    </Container>
-  );
+function addNovaTarefa () {
+  setTarefas([...tarefas, {id: Math.random(), titulo: inputTitulo.current.value, sobre: inputSobre.current.value}])
 }
 
+function deleteTarefa (tarefaId) {
+  const novaTarefa = tarefas.filter( tarefa => tarefa.id !== tarefaId);
+
+  setTarefas(novaTarefa)
+}
+
+
+
+ return (
+    <Container>
+      
+        <ContainerItens>
+        <Image src={Icone}/>
+          <H1>Lista de Tarefas</H1>
+
+          
+          <Input ref={inputTitulo} type='text' placeholder="Título"></Input>
+
+          <Input ref={inputSobre} type='text' id='placeholder-text' placeholder="Sobre"></Input>
+
+          <Button onClick={addNovaTarefa}>Cadastrar Tarefa</Button>
+
+          <ul>
+            {tarefas.map((tarefas) => (
+              <Tarefa key={tarefas.id}>
+                <p>{tarefas.titulo}</p>  <p>{tarefas.sobre}</p>
+                <button onClick={() => deleteTarefa(tarefas.id)}><img src={Lixeira} alt="lata de lixo" /> </button>
+              </Tarefa >
+            ))}
+          </ul>
+          
+        </ContainerItens>
+        
+    </Container>
+  );
+ }
 export default App;
--- src/styles.js
+++ src/styles.js
@@ -1,51 +1,97 @@
 import styled from 'styled-components';
 //estilização aqui 
 
-export const Container= styled.div`
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    gap: 20px;
-    width: 600px;
-    height: 850px;
-    background-color: #1F1E1B;
-    color: #F0E3CA;
-    border-radius: 45px;
-    position: relative;
-    top: 30px;
-    left: 35%;
+
+
+export const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 60px;
+   text-align: center;
+   padding: 50px 36px;
+   height: 200vh;
+   
+`;
+export const Image = styled.img`
+    width: 200px;
+    margin-top: 30px;
+    
+`;
+
+export const ContainerItens = styled.div`
+    background-color: gray;
+    width: 400px;
+    height: 800vh;
+    color: white;
+    border-radius: 30px;
+
 `;
 
 export const H1= styled.h1`
-
+    margin-bottom: 60px;
 `;
 
-export const Label= styled.label`
-    
-`;
+
 export const Input= styled.input`
-    width: 50%;
-    height: 5%;
+    width: 300px;
+    height: 50px;
     border: 2px #FF8303 solid;
     border-radius: 10px;
-    
+    &::placeholder{
+        color: gray;
+        padding: 10px;
+    }
+    outline: none;
+    margin-bottom: 30px;
+    padding: 10px;
 `;
 
+
+
 export const Button = styled.button`
-    width: 50%;
-    height: 5%;
+    width: 300px;
+    height: 50px;
     border: none;
     background-color: #FF8303;
     border-radius: 15px;
     color: #F0E3CA;
     font-size: 20px;
+    cursor: pointer;
+    &:hover{
+    opacity: 0.8;
+}
+    &:active{
+    opacity: 0.5;
+}
 
 `;
 
-export const Image = styled.img`
+export const Tarefa = styled.li`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background: rgba( 255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0,0,0, 0.25);
+    border-radius: 14px;
+    width: 400px;
+    height: 60px;
+    border: none;
+    outline: none;
+    margin-top: 30px;
+    
+    p{
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 28px;
+        color: white;
+    }
 
+    button{
+        background: none;
+        border: none;
+        cursor: pointer;    }
+    
 `;
+
